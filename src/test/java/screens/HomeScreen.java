88db1a92be096fb13a2fbc5ceb0810797494ba75
eugenelukovnikov <virus3625css@yandex.ru>
2026-07-15 22:26:17 +0300

    Заменил способ клика по кнопке "Удалить" на другой.

--- src/test/java/screens/HomeScreen.java
+++ src/test/java/screens/HomeScreen.java
@@ -23,6 +23,8 @@ public class HomeScreen extends BaseScreen {
     final By transactionTitle = By.xpath("//android.widget." +
             "TextView[@resource-id=\"org.secuso.privacyfriendlyfinancemanager:id/textview_transaction_name\"]");
 
+    final By deleteConfirm = By.xpath("//android.widget.Button[@resource-id='android:id/button1']");
+
     private static final int DELETE_BUTTON_X = 1009;
 
     private static final int DELETE_BUTTON_Y = 490;
@@ -52,27 +54,27 @@ public class HomeScreen extends BaseScreen {
     }
     @Step("Свайп влево на элементе")
     public void swipeLeftOnElement() {
-        // 1. Ждем появления элемента и получаем его
+        // Ждем появления элемента и получаем его
 
         WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(constraintLayout));
 
-        // 2. Получаем координаты и размеры элемента
+        // Получаем координаты и размеры элемента
         int elementX = element.getLocation().getX();
         int elementY = element.getLocation().getY();
         int elementWidth = element.getSize().getWidth();
         int elementHeight = element.getSize().getHeight();
 
-        // 3. Вычисляем центр элемента (начало свайпа)
+        // Вычисляем центр элемента (начало свайпа)
         int startX = elementX + (elementWidth / 2);
         int startY = elementY + (elementHeight / 2);
 
-        // 4. Конечная точка: на 300 пикселей левее
+        // Конечная точка: на 300 пикселей левее
         int endX = startX - 300;
 
-        // 5. Создаем "палец" как устройство ввода
+        // Создаем "палец" как устройство ввода
         PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
 
-        // 6. Строим последовательность действий
+        // Строим последовательность действий
         Sequence swipeSequence = new Sequence(finger, 0);
         swipeSequence.addAction(finger.createPointerMove(Duration.ofMillis(0),
                 PointerInput.Origin.viewport(), startX, startY));
@@ -82,19 +84,30 @@ public class HomeScreen extends BaseScreen {
         swipeSequence.addAction(new Pause(finger, Duration.ofMillis(200)));
         swipeSequence.addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
 
-        // 7. Выполняем жест
+        // Выполняем жест
         driver.perform(Arrays.asList(swipeSequence));
     }
 
     @Step("Клик на кнопку 'Удалить'")
     public void clickDeleteButton() {
 
-        PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(constraintLayout));
 
+        int cardY = element.getLocation().getY();
+        int cardHeight = element.getSize().getHeight();
+        int centerY = cardY + (cardHeight / 2);
+
+        //Размер экрана
+        int screenWidth = driver.manage().window().getSize().getWidth();
+
+        //Кликаем с отступом от ширины экрана
+        int clickX = screenWidth - 50;
+
+        PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
         Sequence tap = new Sequence(finger, 1);
-        tap.addAction(finger.createPointerMove(Duration.ofMillis(0), PointerInput.Origin.viewport(), DELETE_BUTTON_X, DELETE_BUTTON_Y));
+        tap.addAction(finger.createPointerMove(Duration.ofMillis(0), PointerInput.Origin.viewport(), clickX, centerY));
         tap.addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
-        tap.addAction(new Pause(finger, Duration.ofMillis(100)));
+        tap.addAction(new Pause(finger, Duration.ofMillis(300)));
         tap.addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
         driver.perform(Arrays.asList(tap));
 
@@ -102,7 +115,6 @@ public class HomeScreen extends BaseScreen {
     @Step("Клик на кнопку 'Подтвердить удаление'")
     public void confirmDelete() {
 
-        By deleteConfirm = By.xpath("//android.widget.Button[@resource-id='android:id/button1']");
         click(deleteConfirm);
 
         System.out.println("Элемент удален!");
